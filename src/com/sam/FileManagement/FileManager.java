be2158e398d2127ec4749908590a35e974f1f270
Sam Lau <sam.suifai.lau@gmail.com>
2014-10-24 04:01:30 -0700

    added higher quality jpeg

--- src/com/sam/FileManagement/FileManager.java
+++ src/com/sam/FileManagement/FileManager.java
@@ -4,6 +4,8 @@ import com.sam.Display.Display;
 import org.apache.commons.io.FilenameUtils;
 
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -14,6 +16,7 @@ import java.io.IOException;
 public class FileManager {
     File directory;
     Display mDisplay;
+    ImageWriter mWriter;
 
     public FileManager (String destination) {
         directory = new File(destination);
@@ -37,7 +40,16 @@ public class FileManager {
         }
         try {
             mDisplay.savingInto(imageFile.getPath());
-            ImageIO.write(image, ext, imageFile);
+
+            if (ext == "jpe?g" || ext == "JPE?G") {
+                mWriter = ImageIO.getImageWritersByFormatName("jpeg").next();
+                ImageWriteParam param = mWriter.getDefaultWriteParam();
+                param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT); // Needed see javadoc
+                param.setCompressionQuality(1.0F); // Highest quality
+                mWriter.write(image);
+            } else {
+                ImageIO.write(image, ext, imageFile);
+            }
         } catch (IOException e) {
             System.err.println("ERROR::cannot write file " + imageFile.getPath());
             e.printStackTrace();
